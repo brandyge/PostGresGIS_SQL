32ae830054014adb66df32a4052b60caa68cafbf
Brandy Geiger <cleverbee@gmail.com>
2022-12-27 13:28:01 -0800

    Update selecteverythingBusinesses.sql

--- selecteverythingBusinesses.sql
+++ selecteverythingBusinesses.sql
@@ -1,8 +1,12 @@
-SELECT bus.business, bus.year_founded, bus.country_code, bus.category_code, countries.continent,  countries.country, categories.category
+/* I am selecting everything and then sorting by most most common categories for the oldest businesses on each continent */ 
+
+SELECT bus.business, bus.year_founded, bus.country_code, bus.category_code, countries.continent,  countries.country,  cat.category
 FROM businesses AS bus
 INNER JOIN countries
 ON bus.country_code = countries.country_code
-INNER JOIN categories 
-ON bus.category_code = categories.category_code
-ORDER BY year_founded; 
+INNER JOIN categories AS cat
+ON bus.category_code = cat.category_code
+
+ORDER BY cat.category
+
 
