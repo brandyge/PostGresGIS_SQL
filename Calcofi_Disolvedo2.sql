448b37fba2473ec2aef79435b4ced3768d1bf64a
Brandy Geiger <cleverbee@gmail.com>
2023-02-13 16:18:12 -0800

    Update Calcofi_Disolvedo2.sql
    noaa calcoffi work Disolved02

--- Calcofi_Disolvedo2.sql
+++ Calcofi_Disolvedo2.sql
@@ -20,6 +20,80 @@ FROM bottles
 
 UPDATE calcofi_cast
 SET object_id = DEFAULT;
+---Find out which of these tables have related columns
+WITH
+  all_columns AS (
+    SELECT table_name, column_name
+    FROM information_schema.columns
+    WHERE table_name IN ('bottles', 'calcofi_cast', 'stationorder')
+  ),
+  all_columns_counts AS (
+    SELECT column_name, count(column_name)
+    FROM all_columns
+    GROUP BY column_name
+  ),
+  common_columns AS (
+    SELECT column_name
+    FROM all_columns_counts
+    WHERE count >= 2
+  )
+SELECT
+  table_name,
+  column_name
+FROM
+  all_columns
+WHERE
+  column_name IN (SELECT column_name FROM common_columns)
+ORDER BY
+  column_name, table_name
+------close out from above query.
+--checking all of line 667050 from bottles and cast table. GOOD QUERY
+SELECT calcofi_cast.sta_id, bottles.sta_id, calcofi_cast.date, calcofi_cast.year, calcofi_cast.ship_name, calcofi_cast.lat_dec, calcofi_cast.lon_dec, bottles.negative_r_depth, bottles."O2ml_L"
+FROM calcofi_cast
+JOIN bottles ON calcofi_cast.sta_id = bottles.sta_id
+WHERE calcofi_cast.sta_id = '066.7 050.0'
+AND bottles.sta_id = '066.7 050.0'
+ORDER BY bottles.negative_r_depth ASC;
+
+select * from calcofi_cast
+--trying to add years 1998-2021 to coincide more with paper.  GOOD QUERY I Need to find a way to ask some question to create a query to display these in a meaninful way.
+SELECT calcofi_cast.sta_id, bottles.sta_id, calcofi_cast.date, calcofi_cast.year, calcofi_cast.distance, calcofi_cast.ship_name, calcofi_cast.lat_dec, calcofi_cast.lon_dec, bottles.negative_r_depth, bottles."O2ml_L"
+FROM calcofi_cast
+JOIN bottles ON calcofi_cast.sta_id = bottles.sta_id
+WHERE calcofi_cast.sta_id = '066.7 050.0'
+AND bottles.sta_id = '066.7 050.0'
+AND calcofi_cast.year BETWEEN 1998 AND 2020
+ORDER BY bottles.negative_r_depth ASC;
+
+---O2ml_L and average distance from shore for each unique value of the negative_r_depth, the lat and long of station, order the results by negative_r_depth asc order. Visualize with the lat and long representing the station and avg_O2ml_L & avg_distance_from_shore.
+--SENT to OUTPUT as a csv and imported into qgis to arcPro-Note this contains an average distance from shore calc.
+SELECT calcofi_cast.sta_id, calcofi_cast.Dry_T, calcofi_cast.geom, bottles.negative_r_depth, AVG(calcofi_cast.distance) as avg_distance_from_shore, AVG(bottles."O2ml_L") as avg_O2ml_L, calcofi_cast.lat_dec, calcofi_cast.lon_dec
+FROM calcofi_cast
+JOIN bottles ON calcofi_cast.sta_id = bottles.sta_id
+WHERE calcofi_cast.sta_id = '066.7 050.0'
+AND bottles.sta_id = '066.7 050.0'
+AND calcofi_cast.year BETWEEN 1998 AND 2020
+GROUP BY calcofi_cast.sta_id, calcofi_cast.Dry_T, bottles.negative_r_depth, calcofi_cast.lat_dec, calcofi_cast.lon_dec, calcofi_cast.geom
+ORDER BY bottles.negative_r_depth ASC;
+--Group the data based on the depth_intervals and distance_intervals
+SELECT depth_intervals, distance_intervals, AVG("O2ml_L"), lat_dec, lon_dec, geom
+FROM (
+  SELECT ROUND(negative_r_depth) AS depth_intervals, ROUND(distance) AS distance_intervals, "O2ml_L", lat_dec, lon_dec, geom
+  FROM calcofi_cast
+  JOIN bottles ON calcofi_cast.sta_id = bottles.sta_id
+  WHERE calcofi_cast.sta_id = '066.7 050.0'
+    AND bottles.sta_id = '066.7 050.0'
+    AND calcofi_cast.year BETWEEN 1998 AND 2020
+) AS subquery
+GROUP BY depth_intervals, distance_intervals, lat_dec, lon_dec, geom
+ORDER BY depth_intervals ASC, distance_intervals ASC;
+
+
+--calculate distint column-IM getting a lot of NULL, not sure but don't use for now
+SELECT DISTINCT ROUND(distance) as distint
+FROM calcofi_cast;
+select distint from calcofi_cast
+
 
 ALTER TABLE calcofi_cast
 ALTER COLUMN object_id OID SET DEFAULT nextval('calcofi_cast_object_id_seq');
@@ -47,7 +121,6 @@ SELECT * FROM bottles WHERE sta_id IN ('066.7 050.0')
 
 select updateGeometrySRID('sql_outputcast066750', 'geom', 4326)
 
-
 --then try the above query, but use the sql_outpustcast006 file and cst_cnt column. Saved csv as testjoin.csv to import
 SELECT sql_outputcast066750.*, bottles.* 
 FROM sql_outputcast066750 
@@ -72,5 +145,5 @@ select bottles.O2ml_L
 FROm bottles
 
 --I am sending the above query to qgis
-
+select * FROM bottles
 
